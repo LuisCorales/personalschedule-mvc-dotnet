45f6635619b60eec28fa4a692557752bacfec436
Diego Hiriart León <hiriart.leon.d@gmail.com>
2021-04-26 14:01:38 -0500

    Clases vacias para DB, navegacion y esteticos
    Se agregaron las clases para las tablas de DB, connection string para Diego en webconfig, clase de base datos, navegación y estetica

---
+++ Hiriart-Corales_MVCWebApp-AgendaPersonal/Hiriart-Corales_MVCWebApp-AgendaPersonal/Models/AgendaPersonalCF_Hiriart-Corales.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;//Necesario pata DbContext y DbSet
+using System.Linq;
+using System.Web;
+
+namespace Hiriart_Corales_MVCWebApp_AgendaPersonal.Models
+{
+    public class AgendaPersonalCF_Hiriart_Corales : DbContext
+    {
+        public DbSet<Evento> Evento { get; set; }
+        public DbSet<Notificacion> Notificacion { get; set; }
+        public DbSet<Memo> Memo { get; set; }
+        public DbSet<Diario> Diario { get; set; }
+        public DbSet<Contacto> Contacto { get; set; }
+
+    }
+}
